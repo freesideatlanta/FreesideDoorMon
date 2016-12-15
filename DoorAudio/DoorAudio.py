69003c85212c6cb1398922ea2970d8d860a2349c
root <scott.mcgraw@gatech.edu>
2016-12-15 04:16:01 +0000

    Remove Wavs

--- DoorAudio/DoorAudio.py
+++ DoorAudio/DoorAudio.py
@@ -4,8 +4,6 @@ import os.path
 import pygame
 
 pygame.init()
-pygame.mixer.init()
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.bind(('<broadcast>', 50505))
@@ -13,12 +11,15 @@ s.bind(('<broadcast>', 50505))
 while True:
 	msg = s.recvfrom(1024)	
 	jmsg = json.loads(msg[0])
+	print msg[0] 
 	if jmsg['location'] == 'frontDoor':
 		if jmsg['action'] == 'cardRead':
 			#check audio fileexists
 			filename = "/usr/local/bin/FreesideDoorMon/DoorAudio/wav/" + jmsg['id'] + ".wav"			
 			if os.path.isfile(filename):
+				pygame.mixer.init()
 				sound = pygame.mixer.Sound(filename)
 				channel = sound.play()
 				while channel.get_busy():
 					pygame.time.delay(100)
+				pygame.mixer.quit()
